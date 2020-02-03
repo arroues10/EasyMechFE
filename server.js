f005598402db62b5969279e6c13df98c326648a9
Solal <arroues10@gmail.com>
2020-02-03 05:20:36 +0200

    03/02/2020 5:20

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-const app = express();
+const app = express(); 
 
 // Serve static files....
 app.use(express.static(__dirname + '/dist/angular-deploy'));
