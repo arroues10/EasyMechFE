96e61e2620dfce9d4ca88e4b3eb92ceece4016f8
Solal <arroues10@gmail.com>
2020-02-03 05:56:46 +0200

    03/02/2020 5:56

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('angular-deploy app is running!');
+    expect(page.getTitleText()).toEqual('easy-mech-fe app is running!');
   });
 
   afterEach(async () => {
