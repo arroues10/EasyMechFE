b60f223106a1c1e5018bf1ef88932fddb2283471
Solal <arroues10@gmail.com>
2020-02-11 06:00:37 +0200

    add description

--- src/app/models/workCard.ts
+++ src/app/models/workCard.ts
@@ -3,6 +3,7 @@ export class WorkCard {
     public constructor(
         public id?: number,
         public plateNumber?: string,
-        public garageCode?: string
+        public garageCode?: string,
+        public description?: string
     ) { }
 }
